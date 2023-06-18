afb645db3de1f0271bedc3bbabbfd3f1bd3888a5
minhbn3107 <109571044+minhbn3107@users.noreply.github.com>
2023-06-18 21:10:14 +0700

    test lint-staged

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.engine(
         extname: '.hbs',
     }),
 );
-app.set('view engine', 'hbs');
+app.set(        'view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 // Routes init
