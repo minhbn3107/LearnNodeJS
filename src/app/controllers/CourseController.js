029c81e2662b0ab731e17dda5128c5b57efbbc4c
minhbn3107 <109571044+minhbn3107@users.noreply.github.com>
2023-06-22 15:35:10 +0700

    add slect option

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -67,13 +67,20 @@ class CourseController {
             .catch(next);
     }
 
-    // trashCourses(req,res,next) {
-    //     Course.findDeleted({})
-    //         .then(courses => {
-    //             const deletedCourses = courses.filter(course => course.deleted === true);
-    //             res.render('me/trash-courses', {
-    //                 courses:multipMongooseToObject(courses) }) })
-    //         .catch(next); }
+    // [POST] /courses/handle-form-actions
+    handleFormActions(req, res, next) {
+        switch (req.body.action) {
+            case 'delete':
+                Course.delete({ _id: { $in: req.body.courseIds } })
+                    .then(() => res.redirect('back'))
+                    .catch(next);
+                break;
+
+            default:
+                res.json({ message: 'Action is invalid' });
+                break;
+        }
+    }
 }
 
 module.exports = new CourseController();
